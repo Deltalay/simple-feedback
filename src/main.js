7161520ff822690c94388fdbe58dc9c823100177
Deltalay <paylayhok168@gmail.com>
2023-01-06 21:18:10 +0700

    Fixed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import Pocketbase from "pocketbase";
 import "./assets/main.css";
-import { createPinia } from 'pinia'
+import { createPinia } from "pinia";
 
 const pb = new Pocketbase("http://127.0.0.1:8090");
 export default pb;
